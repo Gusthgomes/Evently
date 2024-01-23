da2d266e1f7e7131a1d6699b8ba6e721215716a6
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2024-01-23 17:10:41 -0300

    feat: create new components

---
+++ components/shared/Header.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
+
+const Header = () => {
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image
+            src="/assets/images/logo.svg"
+            width={128}
+            height={38}
+            alt="Logo"
+          />
+        </Link>
+
+        <div className="flex w-32 justify-end gap-3">qqc</div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
