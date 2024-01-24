e7302c6ae4104be5e9f3940528b9897be4e3caf7
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2024-01-24 14:15:21 -0300

    feat: add authentication methods

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import React from "react";
 import Image from "next/image";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { Button } from "../ui/button";
 
 const Header = () => {
   return (
@@ -15,7 +17,16 @@ const Header = () => {
           />
         </Link>
 
-        <div className="flex w-32 justify-end gap-3">qqc</div>
+        <div className="flex w-32 justify-end gap-3">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+          </SignedIn>
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
+        </div>
       </div>
     </header>
   );
