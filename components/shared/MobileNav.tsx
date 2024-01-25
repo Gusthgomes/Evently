23dc7ff15dd24ba63b944cd7d7485eb1594bc95e
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2024-01-25 14:33:41 -0300

    feat: add separator component

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -9,6 +9,7 @@ import {
 } from "@/components/ui/sheet";
 import Image from "next/image";
 import { Separator } from "../ui/separator";
+import NavItems from "./NavItems";
 
 const MobileNav = () => {
   return (
@@ -30,7 +31,8 @@ const MobileNav = () => {
             width={128}
             height={38}
           />
-          <Separator />
+          <Separator className="border border-gray-50" />
+          <NavItems />
         </SheetContent>
       </Sheet>
     </nav>
